40c6f4d59708bc2f0b5ae29dabb8f548fe949b8c
Matt O'Connell <mattoconnell408@gmail.com>
2016-08-20 18:37:10 -0400

    Refactor

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ const async = require('async')
 const app = express()
 const SpotifyWebApi = require('spotify-web-api-node')
 const stringSimilarity = require('string-similarity')
-let playlistName = '2016-05-10'
-const samplePlaylist = require(`./data/${playlistName}.json`)
-const ProgressBar = require('progress');
+let playlistName = 'WFUV'
+// const songs = require(`./data/${playlistName}.json`)
+const ProgressBar = require('progress')
+const _ = require('lodash')
+var glob = require("glob")
 
 require('dotenv').load()
 
@@ -19,44 +21,38 @@ app.get('/scrape', function(req, res) {
 	const d = new Date()
 	let dateToUse = null
 	let urls = []
-	for(let i = 100; i > 0; i--) {
+	const count = 100
+	let bar = new ProgressBar(':bar', {total: count});
+
+	for(let i = count; i > 0; i--) {
 		d.setDate(d.getDate() - 1)
 		dateToUse = d.toISOString().split('T')[0]
 		const url = `http://nowplaying.wfuv.org/pleditor/external/playlist.php?id=2&day=${dateToUse}`
 		urls.push(url)
 	}
-	// res.send(queue)
-	const q = async.queue(scrape, 10)
 
-	let results = []
-
-	q.drain = () => {
-		console.log('URLs Scraped....')
-		const writeQ = async.queue(writeToFile, 5)
-		results.forEach(playlist => {
-			writeQ.push(playlist, (title) => {
-				console.log(`Queue: Finished writing ${title}`)
-			})
-		})
-	}
-
-	urls.forEach(url => {
-		q.push(url, (title, songs) => {
-			results.push({title: title, songs: songs})
-			console.log(`Queue: Finished processing ${title}. ${songs.length} songs.`)
-		})
-	})
-
+	async.eachLimit(urls, 10,
+		function(url, callback) {
+			scrape(url, callback)
+		},
+		function(err, data) {
+			if(err) {
+				console.log('Error scraping and stuff', err)
+			} else {
+				console.log('Done', data)
+			}
+		}
+	)
 })
 
 function scrape(url, callback) {
 	request(url, function(error, response, html) {
 		if(!error) {
 			const $ = cheerio.load(html)
-
 			const title = $('#date').val()
 			const songs = []
-			const json = {}
+			console.log(`scraping ${title}`)
+
 
 			// Artists
 			$('.music td:nth-child(2)').each((i, el) => {
@@ -65,39 +61,60 @@ function scrape(url, callback) {
 					title: null
 				}
 			})
-
 			// Titles
 			$('.music td:nth-child(3)').each((i, el) => {
 				songs[i].title = $(el).text()
 			})
-
 			if(!title) {
 				console.log(`Nothin good for ${url}`)
 			}
-
-			callback(title, songs)
-
+			writeToFile(title, songs, callback)
 		} else {
 			console.log(`Error`, error)
 		}
-
 	})
 }
 
-function writeToFile(playlist, callback) {
-	const {title, songs} = playlist
+function writeToFile(title, songs, callback) {
 	fs.writeFile(`data/${title}.json`, JSON.stringify(songs, null, 4), function(err) {
 		console.log(`${title} successfully written!`)
-		callback(title)
+		callback(null, title)
+		if(err) {
+			console.log('error writing to file', err)
+		}
 	})
 }
 
 
-/*
- clean up.
- */
 app.get('/callback', function(req, res) {
 
+	// Get all JSON files
+	glob('data/*.json', {}, function(er, files) {
+		let arr = []
+		async.eachLimit(files, 10,
+			function(item, callback) {
+				fs.readFile(item, 'utf8', function(err, data) {
+					if(err) throw err;
+					let parsedJSON = JSON.parse(data);
+					arr = arr.concat(parsedJSON)
+					callback(null, arr)
+				});
+			},
+			function(err) {
+				if(err) {
+					console.log('Error reading files', err)
+				} else {
+					// End of Read
+					arr = _.uniqBy(arr, 'title')
+					console.log('End', arr)
+					createPlaylist(arr, req, res)
+				}
+			}
+		)
+	})
+})
+
+function createPlaylist(songs, req, res) {
 	const credentials = {
 		clientId: env.CLIENT_ID,
 		clientSecret: env.CLIENT_SECRET,
@@ -121,21 +138,18 @@ app.get('/callback', function(req, res) {
 			playlistId = data.body.id
 			console.log(`Ok. Created playlist: ${playlistId}`)
 
-			// let sampleSong = samplePlaylist[0]
-
-			const bar = new ProgressBar(':bar', {total: samplePlaylist.length});
+			let bar = new ProgressBar(':bar', {total: songs.length});
 
 			/* Loop through playlist */
-			async.eachLimit(samplePlaylist, 1, function(item, callback) {
-					let sampleSong = item
-					console.log('Sample Song', sampleSong)
+			async.eachLimit(songs, 1, function(song, callback) {
+					console.log('Song: ', song)
 					setTimeout(() => {
 						/* Search and add a song */
-						spotifyApi.searchTracks(`track:${sampleSong.title} artist:${sampleSong.artist}`)
+						spotifyApi.searchTracks(`track:${song.title} artist:${song.artist}`)
 							.then(data => {
 								const rawTrack = data.body.tracks.items[0]
 								if(!rawTrack || !rawTrack.artists || !rawTrack.artists[0] || !rawTrack.artists[0].name) {
-									callback(null, sampleSong)
+									callback(null, song)
 									return
 								}
 								const artist = rawTrack.artists[0].name.toLowerCase() // if any artists match
@@ -143,16 +157,16 @@ app.get('/callback', function(req, res) {
 								const trackId = rawTrack.id
 
 								// See if we actually found the right song
-								let artistSimilarity = stringSimilarity.compareTwoStrings(sampleSong.artist, artist)
-								let titleSimilarity = stringSimilarity.compareTwoStrings(sampleSong.title, title)
+								let artistSimilarity = stringSimilarity.compareTwoStrings(song.artist, artist)
+								let titleSimilarity = stringSimilarity.compareTwoStrings(song.title, title)
 
-								titleSimilarity = title.indexOf(sampleSong.title.toLowerCase()) !== -1 ? 1 : titleSimilarity
-								artistSimilarity = artist.indexOf(sampleSong.artist.toLowerCase()) !== -1 ? 1 : artistSimilarity
+								titleSimilarity = title.indexOf(song.title.toLowerCase()) !== -1 ? 1 : titleSimilarity
+								artistSimilarity = artist.indexOf(song.artist.toLowerCase()) !== -1 ? 1 : artistSimilarity
 
 								const totalSim = artistSimilarity + titleSimilarity
 
 								if(totalSim > 1.65) {
-									console.log('yes yes yes', sampleSong.title)
+									console.log('yes yes yes', song.title)
 									return spotifyApi.addTracksToPlaylist('matt_oconnell', playlistId, [`spotify:track:${trackId}`])
 								} else {
 									return new Error('Total sim too low', totalSim)
@@ -184,6 +198,15 @@ app.get('/callback', function(req, res) {
 		.catch(e => {
 			console.log('outside error', e)
 		})
+}
+
+
+/*
+ clean up.
+ */
+app.get('/callback', function(req, res) {
+
+
 })
 
 
